01afee546c2038ad1d8fbdf11804e457c34dea37
KanaAishiro <sonicsoliar@gmail.com>
2025-04-14 20:51:08 +0200

    update CORS 4

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,6 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(5000, "0.0.0.0", () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
